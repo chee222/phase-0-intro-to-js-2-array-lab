4c514d93bd5278b8e9413c4ae2a960a91a918b3d
Chee <ck_white@outlook.com>
2024-01-19 20:57:03 -0500

    arraylab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Milo) {
+    cats.push(Milo);
+  }
+
+  function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+  }
+  function destructivelyRemoveLastCat(Milo, Otis) {
+    cats.pop(Milo, Otis);
+  }
+
+  function destructivelyRemoveFirstCat(Otis, Garfield) {
+    cats.shift(Otis, Garfield);
+  }
+
+  function appendCat(Broom) {
+    return cats.concat(Broom);
+  }
+
+  function prependCat(Arno) {
+    return [Arno, ...cats];
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
